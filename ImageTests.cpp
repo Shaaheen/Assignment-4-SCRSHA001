6ba8785f6729125a22440415fffc71a82421ec7f
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2016-04-19 07:25:48 +0200

    Created unit tests for load and save functions

--- ImageTests.cpp
+++ ImageTests.cpp
@@ -5,12 +5,60 @@
 #include "catch.hpp"
 #include "Image.h"
 #include "ImageTests.h"
+#include <string>
+#include <stdio.h>
 using namespace SCRSHA001;
+using namespace std;
 
-TEST_CASE("Image class loading and saving") {
-    Image *img = new Image();
-    (*img).load("donkey_mask.pgm");
-    delete img;
+TEST_CASE("Image class loading and saving"){
+    SECTION("Loading"){
+        Image *img = new Image();
+
+        (*img).load("donkey_mask.pgm");
+        REQUIRE((*img).getWidth() == 1280);
+        REQUIRE((*img).getHeight() == 1207);
+
+        (*img).load("Lenna_hat_mask.pgm");
+        REQUIRE((*img).getWidth() == 512);
+        REQUIRE((*img).getHeight() == 512);
+
+        (*img).load("Lenna_standard.pgm");
+        REQUIRE((*img).getWidth() == 512);
+        REQUIRE((*img).getHeight() == 512);
+
+        (*img).load("shrek_rectangular.pgm");
+        REQUIRE((*img).getWidth() == 1280);
+        REQUIRE((*img).getHeight() == 1207);
+
+        delete img;
+
+    }
+    SECTION("Saving"){
+        std::string filename= "donkey_mask.pgm";
+        Image *img2 = new Image(filename);
+        (*img2).save("Clone.pgm");
+        delete img2;
+        ifstream ifs(filename, ios::binary|ios::ate);
+        ifstream::pos_type pos = ifs.tellg();
+        int length = pos;
+        char *pChars = new char[length];
+        ifs.read(pChars, length);
+        ifs.close();
+
+        ifstream ifs2("Clone.pgm", ios::binary|ios::ate);
+        ifstream::pos_type pos2 = ifs2.tellg();
+        int length2 = pos;
+        char *pChars2 = new char[length2];
+        ifs2.read(pChars2, length2);
+        ifs2.close();
+        REQUIRE(length == length2);
+        int n = memcmp(pChars,pChars2,length); //Compares two blocks of memory, if n ==0 then identical
+        REQUIRE(n ==0);
+
+
+    }
+
+
 }
 
 TEST_CASE("Image class constructors") {
